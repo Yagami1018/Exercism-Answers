90117fec37a9c9336859071b57df7bccb217c34a
xnak3 <59103074+Yagami1018@users.noreply.github.com>
2026-04-14 21:05:56 -0400

    First Commit

---
+++ csharp/lucians-luscious-lasagna/LuciansLusciousLasagna.cs
@@ -0,0 +1,23 @@
+class Lasagna
+{
+    // TODO: define the 'ExpectedMinutesInOven()' method
+    public int ExpectedMinutesInOven()
+    {
+        return 40;
+    }
+    // TODO: define the 'RemainingMinutesInOven()' method
+    public int RemainingMinutesInOven(int actualMinutes)
+    {
+        return 40-actualMinutes;
+    }
+    // TODO: define the 'PreparationTimeInMinutes()' method
+    public int PreparationTimeInMinutes(int layers)
+    {
+        return layers*2;
+    }
+    // TODO: define the 'ElapsedTimeInMinutes()' method
+    public int ElapsedTimeInMinutes(int layers, int actualMinutes)
+    {
+        return PreparationTimeInMinutes(layers)+actualMinutes;
+    }
+}
